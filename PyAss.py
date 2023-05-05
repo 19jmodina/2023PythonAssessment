8c23404f481319937cafc0cb434ef2264608a255
James <19jmodina@gmail.com>
2023-05-05 12:13:28 +1200

    05/05/23: Basic GUI class w/ support classes

---
+++ PyAss.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import messagebox
+
+class ordering_items:
+    def __init__(self, name, id, price, quantity):
+        pass
+class customer:
+    def __init__(self, name, status):
+        pass
+    
+class ordering_gui:
+    def __init__(self, parent):
+        pass
+
+
+if __name__ == "__main__":
+    root = Tk()
+    show_gui = ordering_gui(root)
+    root.mainloop()
